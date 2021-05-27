474fc0e414b5ae7a35555d60b6a301259a61fb7d
DickDangerJustice <dickdangerjustice@gmail.com>
2021-05-26 22:11:54 -0400

    Test flying ship using ship patches.

---
+++ Airships/Mod.cs
@@ -0,0 +1,49 @@
+﻿// Airships
+// a Valheim mod skeleton using Jötunn
+// 
+// File:    Airships.cs
+// Project: Airships
+
+using BepInEx;
+using HarmonyLib;
+
+namespace Airships
+{
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    [BepInDependency(Jotunn.Main.ModGuid)]
+    //[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
+    internal class Airships : BaseUnityPlugin
+    {
+        public const string PluginGUID = "steamheim.Airships";
+        public const string PluginName = "SteamheimAirships";
+        public const string PluginVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(PluginGUID);
+
+        private void Awake()
+        {
+            // Do all your init stuff here
+            // Acceptable value ranges can be defined to allow configuration via a slider in the BepInEx ConfigurationManager: https://github.com/BepInEx/BepInEx.ConfigurationManager
+            //Config.Bind<int>("Main Section", "Example configuration integer", 1, new ConfigDescription("This is an example config, using a range limitation for ConfigurationManager", new AcceptableValueRange<int>(0, 100)));
+
+            // Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
+            Jotunn.Logger.LogInfo("ModStub has landed");
+
+            harmony.PatchAll();
+        }
+
+        void OnDestroy()
+        {
+            harmony.UnpatchSelf();
+        }
+
+#if DEBUG
+        //private void Update()
+        //{
+        //    if (Input.GetKeyDown(KeyCode.F6))
+        //    { // Set a breakpoint here to break on F6 key press
+        //    }
+        //}
+#endif
+    }
+}
---
+++ Airships/Patches/Ship.cs
@@ -0,0 +1,52 @@
+﻿using Airships.Models;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Airships.Patches
+{
+    static class Ship_Patch
+    {
+        [HarmonyPatch(typeof(Ship), "Update")]
+        class Ship_Update_Patch
+        {
+            private static void Postfix(Ship __instance)
+            {
+                __instance.transform.rotation = Quaternion.Euler(0, __instance.transform.rotation.eulerAngles.y, 0);
+            }
+        }
+
+        [HarmonyPatch(typeof(Ship), nameof(Ship.FixedUpdate))]
+        class Ship_FixedUpdate_Patch
+        {
+            private static void Postfix(Ship __instance)
+            {
+                if (Input.GetKey(KeyCode.UpArrow))
+                {
+                    __instance.GetAdditionalData().VerticalForce += 0.1f;
+                }
+                else if (Input.GetKey(KeyCode.DownArrow))
+                {
+                    __instance.GetAdditionalData().VerticalForce -= 0.1f;
+                }
+
+                Jotunn.Logger.LogInfo(__instance.GetAdditionalData().VerticalForce);
+                //__instance.m_mastObject.transform.position
+                //var center = __instance.m_floatCollider.bounds.center;
+                //center.y = __instance.m_floatCollider.bounds.max.y;
+                __instance.transform.rotation = Quaternion.Euler(0, __instance.transform.rotation.eulerAngles.y, 0);
+                __instance.m_body.AddForceAtPosition(
+                    Vector3.up * Time.fixedDeltaTime * __instance.GetAdditionalData().VerticalForce,
+                    __instance.m_mastObject.transform.position, ForceMode.VelocityChange);
+
+
+                //if (!__instance.GetAdditionalData().isLocked)
+                //    ___displayText.ShowText("Press to LOCK rotation", MyInput.Keybinds["RMB"].MainKey, 2, 0, false);
+                //else
+                //    ___displayText.ShowText("Press to UNLOCK rotation", MyInput.Keybinds["RMB"].MainKey, 2, 0, false);
+
+                //if (MyInput.GetButtonDown("RMB"))
+                //    __instance.GetAdditionalData().isLocked = !__instance.GetAdditionalData().isLocked; // Toggle bool
+            }
+        }
+    }
+}
