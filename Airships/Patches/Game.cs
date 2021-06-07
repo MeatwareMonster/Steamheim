b083efc213cc269124a81cdc5dd1550d3d6cce52
DickDangerJustice <dickdangerjustice@gmail.com>
2021-06-07 13:55:21 -0400

    Comment out currently not working code.

--- Airships/Patches/Game.cs
+++ Airships/Patches/Game.cs
@@ -1,17 +1,27 @@
-﻿using HarmonyLib;
-
-namespace Airships.Patches
+﻿namespace Airships.Patches
 {
     class Game_Patch
     {
-        [HarmonyPatch(typeof(Game), nameof(Game.FindSpawnPoint))]
-        class Game_Interact_Patch
-        {
-            private static void Prefix(Game __instance)
-            {
-                // prevent falling to death on spawn
-                // zoom camera way out when controlling ship
-            }
-        }
+        // Attempt to send the player home if they log out on top of an airship
+        // Not working yet, commenting out code for now
+        //[HarmonyPatch(typeof(Game), nameof(Game.FindSpawnPoint))]
+        //class Game_Interact_Patch
+        //{
+        //    private static void Prefix(Game __instance)
+        //    {
+        //        if (!__instance.m_playerProfile.HaveLogoutPoint()) return;
+
+        //        var logoutPoint = __instance.m_playerProfile.GetLogoutPoint();
+        //        if (ZoneSystem.instance.GetSolidHeight(logoutPoint, out var height))
+        //        {
+        //            Jotunn.Logger.LogInfo($"Login height: {logoutPoint.y}, Solid height: {height}");
+        //            if (logoutPoint.y - height > 10)
+        //            {
+        //                Jotunn.Logger.LogInfo("Player too high.");
+        //                __instance.m_playerProfile.ClearLoguoutPoint();
+        //            }
+        //        }
+        //    }
+        //}
     }
 }
